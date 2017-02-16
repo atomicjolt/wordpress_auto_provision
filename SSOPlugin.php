1997eda811bc312808744b465e6b6350de8783d4
Ubuntu <brady@example.com>
2017-02-16 21:41:33 +0000

    Handling redirects to login within the plugin now (it was causing problems before). Also, changing the autoprovisioning to create a new blog as well as a new user (experimental)

--- SSOPlugin.php
+++ SSOPlugin.php
@@ -6,10 +6,20 @@ This plugin assumes that you have already set up and configured mod_auth_openidc
 and that you're login page is at a location protected by that plugin. This also
 autoprovisions accounts when needed.
 */
+defined('ABSPATH') or die("No script kiddies please!");
+
+function sso_logout(){
+    $url = 'https://wordpress.openlmshost.com/wp-admin?logout=https%3A%2F%2Fwordpress.openlmshost.com%2Floggedout.html';
+    wp_clear_auth_cookie();
+    wp_redirect($url);
+    exit();
+    //var_dump( $current_user);     
+}
+
 function sso_login()
-{
-  $email_address = explode("/",$_SERVER['REMOTE_USER'])[0];
-  if ($email_address == '')
+{  $email_address = explode("/",$_SERVER['REMOTE_USER'])[0];
+   $user_number = explode("@" $email_address)[0];
+   if ($email_address == '')
   {
     echo("Did not obtain user info from SSO server");
     return;
@@ -18,15 +28,16 @@ function sso_login()
   {
     //User hasn't been created yet, auto provision one
     $password = wp_generate_password( 12, true );
-    $user_id = wp_create_user ( $email_address, $password, $email_address );
-    wp_update_user(
-    array(
-      'ID'       => $user_id,
-      'nickname' => $email_address
-    ));
+    $user_id = wpmu_create_user( $user_number, $password, $email_address );
+    //wp_update_user(
+    //array(
+    //  'ID'       => $user_id,
+    //  'nickname' => $email_address
+    //));
     // autoprovisioned as contributers (this can change if needed)
-    $user = new WP_User( $user_id );
-    $user->set_role( 'contributor' );
+    // $user = new WP_User( $user_id );
+    //$user->set_role( 'contributor' );
+    wpmu_create_blog( $user_number, '', '', $user_id , array( 'public' => 1 ), $current_site->id );
   }
   $user = get_user_by('login', $email_address);
   // Redirect URL //
@@ -47,11 +58,15 @@ function sso_login()
 
 function redirectUnauthenticated()
 {
-    if ($_SERVER['REMOTE_USER'] == ''){
-        wp_redirect( 'wordpress.openlmshost.com/login');
+    $current_url = home_url(add_query_arg(array(),$wp->request));
+    if (current_url == 'https://wordpress.openlmshost.com/wp-login.php')
+	exit;
+    if (is_user_logged_in() == false){
+        wp_redirect( 'https://wordpress.openlmshost.com/wp-login.php');
 	exit;
      }
 }
-add_action('wp_head', redirectUnauthenticated);
+add_action('get_header', 'redirectUnauthenticated');
 add_action( 'login_header', 'sso_login' );
+add_action('wp_logout','sso_logout');
 ?>
